387603d969650fd5f1fd4d8aa550b8db26b93951
leskeg <gabriel.leske@gmail.com>
2015-08-24 08:52:08 +0200

    ploting random data

--- demo_plot.py
+++ demo_plot.py
@@ -9,16 +9,13 @@ from ipdb import set_trace
 ax = plt.axes(xlim=(0, 24), ylim=(0, 10))
 ax.grid(True)
 plt.xticks( list(range(0,24)) )
+
 # some X and Y data
+lines_quantity = list(range(10))
 x = [0]
-y = [np.random.random_sample() * 10]
+y = [[0] for _ in lines_quantity]
+li = [ax.plot([],[])[0] for _ in lines_quantity]
 
-# y1 = np.random.random_sample(48,) * 10
-
-li, = ax.plot(x, y)
-# li1, = ax.plot(x, y1)
-# draw and show it
-# fig.canvas.draw()
 plt.ion()
 plt.show()
 
@@ -26,22 +23,20 @@ plt.show()
 while True:
 	try:
 		print(str(x[-1]) + ', ' + str(y[-1]))
-		# y[:-1] = y[1:]
-		# y[-1:] = np.random.random_sample() * 10
+
 		x.append( x[-1]+0.5 )
-		y.append( np.random.random_sample() * 10 )
-		# y1[:-1] = y1[1:]
-		# y1[-1:] = np.random.random_sample() * 10
-		# set the new data
-		li.set_xdata(x)
-		li.set_ydata(y)
-		# li1.set_ydata(y1)
+
+		for line in lines_quantity:
+			y[line].append( np.random.random_sample() * 10 )
+			li[ line ].set_xdata(x)
+			li[ line ].set_ydata(y[line])
+
 		plt.draw()
 
 		if len(x) > 49:
 			x = [0,0.5]
-			y = y[-2:]
-			print(str(x[-2]) + ', ' + str(y[-2]))
+			for line in lines_quantity:
+				y[line] = y[line][-2:]
 
 		time.sleep(0.1)
 		plt.pause(0.01) 
